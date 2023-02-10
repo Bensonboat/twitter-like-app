f966ebd31d9cd6ee231fd7a77434f4f6c133d826
benson <rhualikescode@gmail.com>
2023-02-10 18:31:36 +0800

    Update data

--- client/src/axios/interceptors.ts
+++ client/src/axios/interceptors.ts
@@ -5,7 +5,7 @@ export interface IAxiosError extends AxiosError {
 }
 
 const service = axios.create({
-  baseURL: "http://35.201.232.135:8000",
+  baseURL: "http://localhost:8000",
   timeout: 1000 * 10,
 });
 
--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCcGbFDONo1tvJH-OScWVrEH8KHpYCoSdM",
-  authDomain: "dappio-workshop-twitter.firebaseapp.com",
-  projectId: "dappio-workshop-twitter",
-  storageBucket: "dappio-workshop-twitter.appspot.com",
-  messagingSenderId: "828188464985",
-  appId: "1:828188464985:web:f86344e7974c8f6b17de48",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
